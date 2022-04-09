889e31861aeb0d2a0ab585eda3ebe3219fbacccc
MkMonir <khanmonir1335@gmail.com>
2022-04-09 15:29:35 +0600

    Implementing user authentication and authorization using jsonwebtoken

---
+++ controllers/authController.js
@@ -0,0 +1,105 @@
+const { promisify } = require('util');
+const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const signJwtToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+const createSendJwtToken = (user, statusCode, res) => {
+  const token = signJwtToken(user._id);
+  const cookieOptions = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
+exports.signUp = catchAsync(async (req, res, next) => {
+  const newUser = await User.create(req.body);
+
+  createSendJwtToken(newUser, 201, res);
+});
+
+exports.signIn = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    next(new AppError('Please provide email and password', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    next(new AppError('Incorrect email or password', 401));
+  }
+
+  createSendJwtToken(user, 200, res);
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  // else if (req.cookies.jwt) {
+  //   token = req.cookies.jwt;
+  // }
+
+  if (!token) {
+    next(new AppError('You are not logged in, Please log in first to get access', 401));
+  }
+
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    next(new AppError('The user belonging to this token is no longer exist', 401));
+  }
+
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    next(new AppError('User recently changed password! Please log in again!'), 401);
+  }
+
+  req.user = currentUser;
+  res.locals.user = currentUser;
+  next();
+});
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      next(new AppError('You do not have permission to perform this action', 403));
+    }
+    next();
+  };
+};
+
+exports.updateMyPassword = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  if (!(await user.correctPassword(req.body.currentPassword, user.password))) {
+    next(new AppError('Your current password is incorrect', 401));
+  }
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
+
+  createSendJwtToken(user, 200, res);
+});
