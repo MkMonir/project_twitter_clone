801a72a951850c33bc943e8ded35c2978ecfddc2
MkMonir <khanmonir1335@gmail.com>
2022-04-10 16:29:13 +0600

    Populate user who post new post and likes

--- models/postModel.js
+++ models/postModel.js
@@ -11,11 +11,30 @@ const postSchema = new mongoose.Schema(
       ref: 'User',
     },
     pinned: Boolean,
+    likes: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
 );
+
+postSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'postBy',
+    select: 'username photo',
+  }).populate({
+    path: 'likes',
+    select: 'firstName photo',
+  });
+  next();
+});
 
 const Post = mongoose.model('Post', postSchema);
 
