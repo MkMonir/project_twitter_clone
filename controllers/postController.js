b6997ccded7674857bb54e640361a05e58e0c0b9
MkMonir <khanmonir1335@gmail.com>
2022-04-16 01:22:33 +0600

    Implementing like and unlike functionlity on api

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,20 +4,30 @@ const factory = require('./handlerFactory');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.getPosts = factory.getAll(Post);
-exports.createPost = factory.createOne(Post);
+// exports.createPost = factory.createOne(Post);
+
+exports.createPost = catchAsync(async (req, res, next) => {
+  const doc = await Post.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      data: doc,
+    },
+  });
+});
 
 exports.likeUnlikePost = catchAsync(async (req, res, next) => {
   const postId = req.params.id;
   const userId = req.user.id;
 
+  if (!postId) return;
+
   const likedPost = await Post.findById(postId);
 
-  // const isLiked =
+  const isLiked = likedPost.likes.some((like) => like.id === userId);
 
-  const option = likedPost.likes.some((like) => like.id === userId) ? '$pull' : '$addToSet';
-
-  // Insert user like
-  req.user = await User.findByIdAndUpdate(userId, { [option]: { likes: postId } }, { new: true });
+  const option = isLiked ? '$pull' : '$addToSet';
 
   // Insert post like
   const post = await Post.findByIdAndUpdate(postId, { [option]: { likes: userId } }, { new: true });
